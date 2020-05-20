31bd6bd99dedd880af432e226abe2c21c2726ff5
Mathilde <mtmrkaploun@gmail.com>
2020-05-20 23:11:41 +0200

    Command line interface which takes direct text or text files and can output to a file

--- main.py
+++ main.py
@@ -5,12 +5,29 @@ translate = boto3.client(service_name='translate')
 
 
 @click.command()
-@click.argument('untranslated_text')
-def translator(untranslated_text):
+@click.argument('direct_input', required=False)
+@click.option('--file', '-f', default=None, help='Takes text file as input')
+@click.option('--output', '-o', default=None, help='Outputs to the given file')
+def translator(direct_input, file, output):
+
+    if file is not None:
+        with open(file) as file_to_translate:
+            untranslated_text = file_to_translate.read()
+        translated = translate_to_jp(untranslated_text)
+    else:
+        translated = translate_to_jp(untranslated_text)
+
+    if output is None:
+        click.echo(translated)
+    else:
+        with open(output, 'w') as output_file:
+            output_file.write(translated)
+
+
+def translate_to_jp(text):
     translated = translate.translate_text(
-        Text=untranslated_text, SourceLanguageCode="ja", TargetLanguageCode="en")['TranslatedText']
-    click.echo('Here is your translated text :')
-    click.echo(translated)
+        Text=text, SourceLanguageCode="ja", TargetLanguageCode="en")['TranslatedText']
+    return translated
 
 
 if __name__ == '__main__':
